9008472da6018cceedd5fad24c0460a0a216ec26
Abu Ehab <MyGlassBox@Hotmail.Com>
2014-09-17 07:49:16 +0300

    New Edit Of Group Peremissions By Abu Ehab

--- UcasProjectManagement/Ucas.Data/CommandClass/PeremissionsHolderClass.cs
+++ UcasProjectManagement/Ucas.Data/CommandClass/PeremissionsHolderClass.cs
@@ -7,33 +7,54 @@ namespace Ucas.Data.CommandClass
 {
     public    class PeremissionsHolderClass
     {
-        public static int _GroupID;
 
-      public static int xAddUser;
-      public static int xUpDateUser;
-      public static int xDeleteUser;
+    #region "   List Of Peremissions           "
+      public static int _GroupID;
 
-      public static int xAddProject;
-      public static int xDisplayExpenses;
-      public static int xCanPrint;
+      public static int _AddUser;
+      public static int _UpDateUser;
+      public static int _DeleteUser;
 
-      public static int xAddEmployee;
-      public static int xDeleteEmployee;
-      public static int xEditEmployee;
+      public static int _AddProject;
+      public static int _DisplayExpenses;
+      public static int _CanPrint;
+
+      public static int _AddEmployee;
+      public static int _DeleteEmployee;
+      public static int _EditEmployee;
 
 
-      public static int xAddSuppliers;
-      public static int xEditSuppliers;
-      public static int xDeleteSuppliers;
+      public static int _AddSuppliers;
+      public static int _EditSuppliers;
+      public static int _DeleteSuppliers;
 
-      public static int xAddFinncers;
-      public static int xEditFinncers;
-      public static int xDeleteFinncers;
+      public static int _AddFinncers;
+      public static int _EditFinncers;
+      public static int _DeleteFinncers;
 
-    //   --- {Current User's  Data }
+        //=============================
+
+      static UcasProEntities db = new UcasProEntities();
+      public static List<PeremissionsTb> GetAllGroupPeremissionsByGroupID(int GID)
+      {
+          db = new UcasProEntities();
+          db.Configuration.LazyLoadingEnabled = false;
+          db.Configuration.ProxyCreationEnabled = false;
+         var  ListOfPeremissions = (from g in db.PeremissionsTbs
+                                where g.GroupID == GID
+                                select g).ToList();
+         return ListOfPeremissions;
+      }
+
+      #endregion 
+
+      #region "  {Current User's  Data }       "
+      //   --- 
       public static int xCurrentUserID;
       public static string xCurrentUserName;
-         public static  string xCurrentUserPass ;
+      public static  string xCurrentUserPass;
+
+      #endregion 
 
     }
 }
