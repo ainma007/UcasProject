65b172191cc3c404c2e50dc3fa010137b9557c5d
ainma007 <b-sword@outlook.com>
2014-09-21 18:22:51 +0300

    Mypro

--- UcasProjectManagement/Ucas.Data/UcasPro.Designer.cs
+++ UcasProjectManagement/Ucas.Data/UcasPro.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Abu Ehab\Documents\GitHub\UcasProject\UcasProjectManagement\Ucas.Data\UcasPro.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Heroo\Documents\GitHub\UcasProject\UcasProjectManagement\Ucas.Data\UcasPro.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
