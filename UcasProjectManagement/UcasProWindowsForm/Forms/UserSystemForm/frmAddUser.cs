6e2801ce007e8b16215b72341545170687e873e7
ainma007 <b-sword@outlook.com>
2014-09-21 19:51:43 +0300

    FillCombo

--- UcasProjectManagement/UcasProWindowsForm/Forms/UserSystemForm/frmAddUser.cs
+++ UcasProjectManagement/UcasProWindowsForm/Forms/UserSystemForm/frmAddUser.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using Telerik.WinControls;
+using Telerik.WinControls.Data;
 using Ucas.Data.CommandClass;
 namespace UcasProWindowsForm.Forms.UserSystemForm
 {
@@ -15,14 +16,50 @@ namespace UcasProWindowsForm.Forms.UserSystemForm
         {
             InitializeComponent();
         }
-        #region " Load All Groups & Users If It Exiest "
+        #region " Load All Groups & Employee If It Exiest "
         private void PopulateComboxes()
         {
+            ///GetAllEmployeeCombo
+            this.EmployeeComboBox.AutoFilter = true;
+            this.EmployeeComboBox.ValueMember = "ID";
+            this.EmployeeComboBox.DisplayMember = "EmployeeName";
+
+
+            FilterDescriptor filter = new FilterDescriptor();
+            filter.PropertyName = this.EmployeeComboBox.DisplayMember;
+            filter.Operator = FilterOperator.Contains;
+            this.EmployeeComboBox.EditorControl.MasterTemplate.FilterDescriptors.Add(filter);
+
+         
+            employeeBindingSource.DataSource = EmployeeCmd.GetAll();
+            
+            
+            /////GetAllGroupComboBox
+            this.GroupComboBox.AutoFilter = true;
+            this.GroupComboBox.ValueMember = "ID";
+            this.GroupComboBox.DisplayMember = "GroupName";
+
+            FilterDescriptor filter2 = new FilterDescriptor();
+            filter2.PropertyName = this.GroupComboBox.DisplayMember;
+            filter2.Operator = FilterOperator.Contains;
+            this.GroupComboBox.EditorControl.MasterTemplate.FilterDescriptors.Add(filter2);
+            groupsTbBindingSource.DataSource = GroupCmd.GetAllGroups();
+            
             
         }
         #endregion 
 
         private void frmAddUser_Load(object sender, EventArgs e)
+        {
+            PopulateComboxes();
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("" + GroupComboBox.ToString());
+        }
+
+        private void EmployeeComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
